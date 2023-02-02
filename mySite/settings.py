9639b08139b274a81ecb4d4417410e0248a65346
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-02 15:12:12 -0600

    Update settings.py
    1

--- mySite/settings.py
+++ mySite/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ["goldfish-app-xamh4.ondigitalocean.app"]
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
