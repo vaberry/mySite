4f8d43e339c9dcdbb634fb1a822896fb6d9ee325
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-02 20:57:38 -0600

    Update settings.py

--- mySite/settings.py
+++ mySite/settings.py
@@ -1,29 +1,12 @@
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-
 import os
-import sys
-import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# project_name = str(BASE_DIR).rsplit('\\', 1)[-1]
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-# DEBUG = True
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-# ALLOWED_HOSTS = ["goldfish-app-xamh4.ondigitalocean.app"]
-
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
-
-# Application definition
 
 INSTALLED_APPS = [
     'main',
@@ -46,7 +29,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# ROOT_URLCONF = f'{project_name}.urls'
 ROOT_URLCONF = 'mySite.urls'
 
 TEMPLATES = [
@@ -65,13 +47,7 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = f'{project_name}.wsgi.application'
 WSGI_APPLICATION = 'mySite.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 
 # if DEVELOPMENT_MODE is True:
 #     DATABASES = {
@@ -86,10 +62,6 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 #     DATABASES = {
 #         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 #     }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -106,27 +78,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
