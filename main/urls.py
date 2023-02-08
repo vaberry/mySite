138c2870e455948e92f736b1fe5ececa87aae14a
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-07 22:13:39 -0500

    1

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.Index.as_view(),name='index') 
+   path('',views.Index.as_view(),name='index'),
+   path('test/', views.Test.as_view(), name='test'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.views.generic import View,TemplateView
 
 class Index(TemplateView):
     template_name='index.html'
+
+class Test(TemplateView):
+    template_name='test.html'
