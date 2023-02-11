7d347e0d72faa97e64338ed31ba86d7ea13a8445
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-11 00:18:45 -0500

    1

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
    path('',views.Index.as_view(),name='index'),
-   # path('lookup/', views.Lookup.as_view(), name='lookup'),
+   path('lookup/', views.Lookup.as_view(), name='lookup'),
 ]
