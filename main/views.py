57a7f5d40b246d2eefc4496046e989bc4b4b8eaa
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-11 17:47:08 -0500

    1

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View,TemplateView
 import requests
 import json
 import os
-from mySite.settings import APP_ID, APP_KEY
+from mySite.settings import LOOKUP_APP_ID, LOOKUP_APP_KEY
 
 class Index(TemplateView):
     template_name='index.html'
@@ -14,12 +14,10 @@ class Lookup(View):
 
             if 'word' in request.GET:
                 word = request.GET.get('word')
-                # APP_ID = os.getenv("APP_ID")
-                # APP_KEY = os.getenv("APP_KEY")
                 endpoint = "entries"
                 language_code = "en-us"
                 url = "https://od-api.oxforddictionaries.com/api/v2/" + endpoint + "/" + language_code + "/" + word.lower()
-                response = requests.get(url, headers = {"app_id": str(APP_ID), "app_key": str(APP_KEY)})
+                response = requests.get(url, headers = {"app_id": str(LOOKUP_APP_ID), "app_key": str(LOOKUP_APP_KEY)})
                 r = json.loads(response.text)
                 definition = r["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]             
 
