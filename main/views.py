6510129bdc5d3d44f46a18d0b95da73ce161b1d1
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-11 00:49:09 -0500

    1

--- main/views.py
+++ main/views.py
@@ -1,23 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import View,TemplateView
-from pydictionary import Dictionary
+# from pydictionary import Dictionary
 
 class Index(TemplateView):
     template_name='index.html'
 
-class Lookup(View):
-    def get(self, request, *args, **kwargs):
-        if request.method == "GET":
-            if 'word' in request.GET:
-                word = request.GET.get("word")
-                response = Dictionary(word).meanings()
-                if len(response) == 0:
-                    response.append('Could not find that word...')
+# class Lookup(View):
+#     def get(self, request, *args, **kwargs):
+#         if request.method == "GET":
+#             if 'word' in request.GET:
+#                 word = request.GET.get("word")
+#                 response = Dictionary(word).meanings()
+#                 if len(response) == 0:
+#                     response.append('Could not find that word...')
 
-                context = {
-                    'word' : word,
-                    'definition' : response
-                }
-                return render(request, template_name="lookup.html", context=context)
-        return render(request, template_name="lookup.html")
+#                 context = {
+#                     'word' : word,
+#                     'definition' : response
+#                 }
+#                 return render(request, template_name="lookup.html", context=context)
+#         return render(request, template_name="lookup.html")
