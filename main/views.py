d7361b2c622365a23604b0a21ba0b09562913265
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-11 17:07:14 -0500

    1

--- main/views.py
+++ main/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, redirect
 from django.views.generic import View,TemplateView
 import requests
 import json
-from config import APP_ID, APP_KEY
+# from config import APP_ID, APP_KEY
 import os
 # from pydictionary import Dictionary
+import configparser
 
 class Index(TemplateView):
     template_name='index.html'
@@ -15,21 +16,21 @@ class Lookup(View):
         if request.method == "GET":
             if 'word' in request.GET:
                 word = request.GET.get('word')
-                APP_ID = os.getenv("APP_ID")
-                APP_KEY = os.getenv("APP_KEY")
+                config = configparser.ConfigParser()
+                config.read('.env')
+                APP_KEY = config.get('APP', 'APP_KEY')
+                APP_ID = config.get('APP', 'APP_ID')
                 endpoint = "entries"
                 language_code = "en-us"
                 word_id = "example"
                 url = "https://od-api.oxforddictionaries.com/api/v2/" + endpoint + "/" + language_code + "/" + word.lower()
-                response = requests.get(url, headers = {"app_id": APP_ID, "app_key": APP_KEY})
-                print(type(response.text))
-                print(type(json.loads(response.text)))
+                response = requests.get(url, headers = {"app_id": str(APP_ID), "app_key": str(APP_KEY)})
                 r = json.loads(response.text)
-                print(r["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"])                
+                definition = r["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]             
 
                 context = {
                     'word' : word,
-                    'definition' : r
+                    'definition' : definition,
                 }
                 return render(request, template_name="lookup.html", context=context)
         return render(request, template_name="lookup.html")
