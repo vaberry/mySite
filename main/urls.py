6284d72c68875eece8146bb9428912773af511ec
vaberry <64152604+vaberry@users.noreply.github.com>
2023-02-10 23:43:19 -0500

    1

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
    path('',views.Index.as_view(),name='index'),
-   path('test/', views.Test.as_view(), name='test'),
+   path('lookup/', views.Lookup.as_view(), name='lookup'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic import View,TemplateView
-# Create your views here.
+from pydictionary import Dictionary
 
 class Index(TemplateView):
     template_name='index.html'
 
-class Test(TemplateView):
-    template_name='test.html'
+class Lookup(View):
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            if 'word' in request.GET:
+                word = request.GET.get("word")
+                response = Dictionary(word).meanings()
+                if len(response) == 0:
+                    response.append('Could not find that word...')
+
+                context = {
+                    'word' : word,
+                    'definition' : response
+                }
+                return render(request, template_name="lookup.html", context=context)
+        return render(request, template_name="lookup.html")
